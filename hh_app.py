310f7cd2ed7008d22e04d8fe3136dd1231190ed2
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-01-13 14:27:08 +0300

    refactor hh_app.py

--- hh_app.py
+++ hh_app.py
@@ -42,37 +42,3 @@ def predict_rub_salary(vacancy):
     if salary_to:
         return 0.8 * salary_to
 
-
-# def get_sallary(records):
-#     processed = 0
-#     mid_salary = 0
-#     for vacancy in records['items']:
-#         salary = predict_rub_salary(vacancy)
-#         if salary:
-#             mid_salary+=salary
-#             processed+=1
-#     mid_salary /= processed
-#     return mid_salary, processed
-
-
-# def get_sallary_by_language(language):
-#     records = get_found_records(language)
-#     mid_salary, processed =get_sallary(records)
-#     return {
-#         "vacancies_found": records['found'],
-#         "vacancies_processed": processed,
-#         "average_salary": mid_salary
-#     }
-
-# print(f'vacansies in Moscow last 30 days = {get_found_records(30)}')
-# print(f'vacansies in Moscow all time = {get_found_records()}')
-#
-# popular_languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++','C#', 'C', 'Go' ]
-# vacancies_by_language={}
-# for language in popular_languages:
-#     vacancies_by_language[language] = get_sallary_by_language(language)
-# print(vacancies_by_language)
-
-# get_sallary_by_language('Python')
-
-# print(get_sallary_by_language("Python"))
