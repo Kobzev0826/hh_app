497a6555c77cd93abcb63c169f7b10b263e5634f
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-01-13 14:26:47 +0300

    refactor super_job_api.py

--- super_job_api.py
+++ super_job_api.py
@@ -1,7 +1,6 @@
 import os
 import requests
-import dotenv
-from terminaltables import AsciiTable
+
 def get_found_records(keyword):
 
     Secret_Key = os.environ['SuperJob']
@@ -26,7 +25,6 @@ def get_found_records(keyword):
 
         all_data['found'] = response.json()['total']
         all_data["items"] += response.json()['objects']
-        # print(f'{len(all_data["items"])}/ {all_data["found"]}')
         page += 1
         payload ['page'] = page
         more = response.json()['more']
@@ -35,7 +33,7 @@ def get_found_records(keyword):
 
 
 def predict_rub_salary_for_superJob(vacancy):
-    # print (vacancy)
+
     salary_from = vacancy['payment_from']
     salary_to = vacancy['payment_to']
     if salary_from and salary_to:
@@ -46,48 +44,3 @@ def predict_rub_salary_for_superJob(vacancy):
         return 0.8 * salary_to
     return None
 
-
-# def get_sallary(records):
-#     processed = 0
-#     mid_salary = 0
-#     for vacancy in records['items']:
-#         salary = predict_rub_salary_for_superJob(vacancy)
-#         if salary:
-#             mid_salary+=salary
-#             processed+=1
-#     mid_salary /= processed
-#     return mid_salary, processed
-
-
-# def get_sallary_by_language(language):
-#     records = get_found_records(language)
-#     mid_salary, processed =get_sallary(records)
-#     # return {
-#     #     "vacancies_found": records['found'],
-#     #     "vacancies_processed": processed,
-#     #     "average_salary": mid_salary
-#     # }
-#     return [language, records['found'],processed, mid_salary ]
-
-
-
-# if __name__ == '__main__':
-#     dotenv.load_dotenv()
-#     popular_languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go']
-#     # vacancies_by_language = {}
-#     table_data = [
-#         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', "Средняя зарплата"],
-#     ]
-#     for language in popular_languages:
-#         # vacancies_by_language[language] = get_sallary_by_language(language)
-#         table_data.append(get_sallary_by_language(language))
-
-    # print(vacancies_by_language)
-
-
-    table = AsciiTable(table_data, 'SuperJob.ru')
-    print(table.table)
-
-    # for vacancy in get_vacancyes('Программист'):
-    #     salary = predict_rub_salary_for_superJob(vacancy)
-    #     print(f"{vacancy['profession']}, {vacancy['town']['title']}, salary = {salary}")
