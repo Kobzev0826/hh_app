14bdcf9141273129e1d4617f683ede6161846cd0
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-01-15 11:57:40 +0300

    refactor main.py divide zero check

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ def get_sallary(records, predict_func):
         if salary:
             mid_salary+=salary
             processed+=1
-    mid_salary /= processed
+    if processed:
+        mid_salary /= processed
+
     return mid_salary, processed
 
 
-def get_sallary_by_language(language, found_records, predict_func):
-    records = found_records(language)
-    mid_salary, processed =get_sallary(records,predict_func)
-    return [language, records['found'],processed, mid_salary ]
+def get_language_statistics(language, found_records, predict_func):
+    vacancies = found_records(language)
+    mid_salary, processed =get_sallary(vacancies,predict_func)
+    return [language, vacancies['found'],processed, mid_salary ]
 
 
 if __name__ == '__main__':
@@ -31,8 +33,8 @@ if __name__ == '__main__':
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', "Средняя зарплата"],
     ]
     for language in popular_languages:
-        table_superjob.append(get_sallary_by_language(language, super_job_api.get_found_records, super_job_api.predict_rub_salary_for_superJob))
-        table_hh.append(get_sallary_by_language(language, hh_app.get_found_records, hh_app.predict_rub_salary))
+        table_superjob.append(get_language_statistics(language, super_job_api.get_found_records, super_job_api.predict_rub_salary_for_superJob))
+        table_hh.append(get_language_statistics(language, hh_app.get_found_records, hh_app.predict_rub_salary))
 
     print(AsciiTable(table_superjob, 'SuperJob.ru').table)
     print(AsciiTable(table_hh, 'HeadHunter.ru').table)
